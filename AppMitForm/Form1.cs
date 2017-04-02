147994509245d2861f0908bc6deeb4992d2d3748
Danilo <danilo.dionisia@yahoo.com.br>
2017-04-02 20:52:52 -0300

    teste de form

--- AppMitForm/Form1.cs
+++ AppMitForm/Form1.cs
@@ -19,6 +19,7 @@ namespace AppMitForm
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //exibe mensagem na tela
             MessageBox.Show("Hello world!!");
         }
     }
